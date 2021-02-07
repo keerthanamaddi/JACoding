2cb564081a46c009c266dd156177107bc835db2a
keerthanamaddi <keerthana.maddi@gmail.com>
2021-02-08 10:40:30 +1100

    Implemented IMemoryCache
    im memory caching

--- backend/backend/Controllers/MatchingCandidatesController.cs
+++ backend/backend/Controllers/MatchingCandidatesController.cs
@@ -6,6 +6,7 @@ using backend.Model;
 using backend.Service.Data;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.Logging;
 
 namespace backend.Controllers
@@ -16,17 +17,29 @@ namespace backend.Controllers
     {
         private readonly MatchingCandidatesData _matchingCandidatesData;
         private readonly ILogger<MatchingCandidatesController> _logger;
+        private IMemoryCache _memoryCache;
 
-        public MatchingCandidatesController(ILogger<MatchingCandidatesController> logger, MatchingCandidatesData matchingcandidates)
+        public MatchingCandidatesController(ILogger<MatchingCandidatesController> logger, MatchingCandidatesData matchingcandidates, IMemoryCache cache)
         {
             _matchingCandidatesData = matchingcandidates;
             _logger = logger;
+            _memoryCache = cache;
         }
 
         [HttpGet]
         public async Task<List<MatchingCandidate>> Get()
         {
-            return await _matchingCandidatesData.GetMatchingCandidates();
+            List<MatchingCandidate> cacheEntry;
+            if(!_memoryCache.TryGetValue("matchingCandidates", out cacheEntry))
+            {
+                cacheEntry = await _matchingCandidatesData.GetMatchingCandidates();
+
+                var cacheEntryOptions = new MemoryCacheEntryOptions().SetSlidingExpiration(TimeSpan.FromMinutes(5));
+
+                _memoryCache.Set("matchingCandidates", cacheEntry, cacheEntryOptions);
+            }
+            return cacheEntry;
+            //return await _matchingCandidatesData.GetMatchingCandidates();
         }
     }
 }
